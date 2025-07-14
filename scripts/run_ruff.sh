5887fa842f67416fc64ea82550018f1d03289835
Bert <bert.k@hotmail.com>
2025-07-14 08:33:53 +0000

    check tests

--- scripts/run_ruff.sh
+++ scripts/run_ruff.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source "$VIRTUAL_ENV/bin/activate"
-ruff format --check custom_components/weishaupt_modbus/
-ruff check custom_components/weishaupt_modbus/
+ruff format --check custom_components/weishaupt_modbus/ tests/
+ruff check custom_components/weishaupt_modbus/ tests/
